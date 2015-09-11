dab54943237feaec5cb95c4fadd98bb017704db9
mustafa parlaktuna <mparlaktuna@gmail.com>
2015-09-11 06:14:45 +0300

    added logger output

---
+++ src/logger_output.py
@@ -0,0 +1,23 @@
+__author__ = 'mustafa'
+
+from PySide.QtGui import *
+from PySide.QtCore import *
+from data_writer import print_data
+
+class LogData(QWidget):
+    """
+    shows all data inside data store object
+    """
+    def __init__(self):
+        QWidget.__init__(self)
+        self.setWindowTitle('Logger')
+        self.textbox = QTextEdit()
+        self.textbox.setReadOnly(True)
+        self.layout = QHBoxLayout()
+        self.layout.addWidget(self.textbox)
+        self.setLayout(self.layout)
+
+    def write(self, message):
+        self.textbox.moveCursor(QTextCursor.End)
+        self.textbox.insertPlainText(message)
+
